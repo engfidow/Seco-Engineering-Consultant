2bd8aa99b02194ab297d8b0654f57b221761a93c
abdallayusufahmed@gmail.com <abdallayusufahmed@gmail.com>
2023-06-06 22:48:54 +0300

    Change footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <>
-      <footer className=" shadow bg-slate-50 mt-[425px]">
+      <footer className=" shadow bg-slate-50 mt-[280px]">
         <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
           <div className="sm:flex sm:items-center sm:justify-between">
             <Link to="/" className="flex items-center mb-4 sm:mb-0">
