59af01a085cd5c67356ce57f56f7e9db9bbfbf4a
abdallayusufahmed@gmail.com <abdallayusufahmed@gmail.com>
2023-06-06 22:53:05 +0300

    Change herro

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -49,7 +49,7 @@ const Hero = () => {
                 repeat: Infinity,
                 repeatType: "loop",
               }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
+              className='w-3 h-3 rounded-full bg-secondary mb-1 sm:hidden'
             />
           </div>
         </a>
